66a2cc05050055e4e5fd489f0c1d2f23bd6c6ec5
Johnathan Beal <johnathanbeal@gmail.com>
2020-06-06 22:14:07 -0400

    got debugging to work, trying to see if fetch call works

--- API/wwwroot/js/twitterSearchApi.js
+++ API/wwwroot/js/twitterSearchApi.js
@@ -1,6 +1,7 @@
-let uri = "https://api.twitter.com/1.1/search/tweets.json?george+floyd"
+let uri = "https://api.twitter.com/1.1/search/tweets.json"
 
 function getTweets() {
+    uri = uri + "?" + getSearchText();
     fetch(uri)
         .then(function (response) {
             return response.json();
@@ -11,4 +12,16 @@ function getTweets() {
         .catch(function (err) {
             console.log("Something went wrong!", err);
         })
+}
+
+//function getSearchParameter() {
+//    return document.getElementById("submit").addEventListener("click", getSearchText);
+//}
+
+function getSearchText() {
+    console.log("test");
+    console.log(document.getElementById("search").textContent);
+    console.log(document.getElementById("search").innerText);
+    console.log(document.getElementById("search"));
+    return document.getElementById("search").value;
 }
