986a3528390739990100bd0682f08c3a064f82bd
Johnathan Beal <johnathanbeal@gmail.com>
2020-06-07 20:30:19 -0400

    can't figure out how to pass secrets.json values to twitterSearchAPI.js

--- API/wwwroot/js/twitterSearchApi.js
+++ API/wwwroot/js/twitterSearchApi.js
@@ -3,6 +3,9 @@ let username = "user";
 let password = "passwd";
 
 function getTweets() {
+
+    var token = getToken();
+
     uri = uri + "?" + getSearchText();
     fetch(uri)
         .then(function (response) {
@@ -29,24 +32,83 @@ function getSearchText() {
 }
 
 function basicAuth() {
-    let base64 = require('base-64');
+    try {
+        let base64 = require('base-64');
+        console.log(base64);
 
-    let url = "https://api.twitter.com/oauth2/token";
-    username = TwitterUsername();
-    password = TwitterPassword();
+        let url = "https://api.twitter.com/oauth2/token";
+        console.log(url);
+        username = TwitterUsername();
+        console.log(username);
+        password = TwitterPassword();
+        console.log(password());
 
-    let headers = new Headers();
+        let headers = new Headers();
 
-    headers.append('Authorization', 'Basic ' + base64.encode(username + ":" + password));
+        headers.set('Authorization', 'Basic ' + base64.encode(username + ":" + password));
 
+        fetch(url, {
+            method: 'GET',
+            headers: headers,
+        })
+            .then(response => response.json())
+            .then(json => console.log(json));
+
+        function parseJSON(response) {
+            return response.json()
+        }
+    }
+    catch (error)
+    {
+        console.error(error);
+        console.log(error);
+    }
+    
+
+    //https://stackoverflow.com/questions/43842793/basic-authentication-with-fetch
 }
 
 function TwitterUsername() {
-    var authInfo = "<%= TwitterTokenAuthInfo %>";
-    return authInfo.TwitterUsername 
+    try {
+        var secret = require("..\secrets.json");
+    }
+    catch (error)
+    {
+        console.error(error);
+        console.log(error);
+    }
+    console.log(secret.Twitter.Username);
+    //var authInfo = "<%= TwitterTokenAuthInfo %>";
+    //var twitterUsername = Configuration["Twitter:Username"];
+    return twitterUsername 
 }
 
 function TwitterPassword() {
-    var authInfo = "<%= TwitterTokenAuthInfo %>";
-    return authInfo.TwitterPassword;
+    //var authInfo = "<%= TwitterTokenAuthInfo %>";
+    var secret = require("..\secrets.json");
+
+    console.log(secret.Twitter.Password);
+    //var twitterPassword = Configuration["Twitter:Password"];
+    return twitterPassword;
+}
+
+function getToken() {
+    let url = "https://api.twitter.com/oauth2/token";
+    let username = TwitterUsername();
+    let password = TwitterPassword();
+
+    let headers = {
+        'Authorization': 'Basic ' + base64.encode(username + ":" + password)
+    };
+
+    let options = {
+        method: 'POST',
+        headers: headers
+    }
+    fetch(url, options)
+        .then((httpResponse) => {
+            console.log(httpResponse.json);
+            console.log(httpResponse.text);
+            return httpResponse.json;
+        });
 }
