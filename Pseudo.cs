eec3f73db75e2bf62f4bede54add2ba1626ae9cb
Johnathan Beal <johnathanbeal@gmail.com>
2020-06-13 13:20:11 -0400

    added pseudo code

---
+++ Pseudo.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sankyo.Sue_Dough
+{
+    public class PseudoCode
+    {
+        public void CallThirdPartyApi()
+        {
+            //Send Twitter Username and Password to Token endpoint
+
+            //Get Twitter Bearer Token
+
+            //Use Twitter Token to Call Search API
+
+            //Make Returned Data Available
+
+        }
+
+        public void CallInternalApi()
+        {
+            // Get the data returned by the CallThirdPartyApi endpoint
+
+            // Make this data available for Fetch API
+        }
+
+        public void UseFetchApiToCallInternalApi()
+        {
+            //Use Fetch API to get Data from Interal API
+
+            //Fetch API displays data on Website
+        }
+    }
+}
