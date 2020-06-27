da2c131cc65f4e66653867819a54d3d7f3d937b6
Johnathan Beal <johnathanbeal@gmail.com>
2020-06-27 14:01:45 -0400

    Fetch progress

--- API/wwwroot/js/twitterSearchApi.js
+++ API/wwwroot/js/twitterSearchApi.js
@@ -1,14 +1,44 @@
-let uri = "https://localhost:44361/api/tweets/lincoln"
+let uri = "https://localhost:44361/api/tweets/lincoln";
+let url = "api/tweets/";
 
-async function TryFetch() {
-    let response = await fetch(uri);
+async function TryFetch(search) {
+    const data;
+    url = url + search;
+    console.log("start");
+    try {
+        fetch(url).then(r => r.json());
 
-    if (response.ok) { // if HTTP-status is 200-299
-        // get the response body (the method explained below)
-        let json = await response.json();
-        console.log(json);
-        alert("Response is " + json);
-    } else {
-        alert("HTTP-Error: " + response.status);
+        data = await fetch(url)
+            .then(response => response.json())
+            .then(data => console.log(data));
+
+        //let response = await fetch(uri);
+        //console.log("call to fetch api with full url");
+        //if (response.ok) { // if HTTP-status is 200-299
+        //    // get the response body (the method explained below)
+        //    let json = await response.json();
+        //    console.log(json);
+        //    alert("Response is " + json);
+        //} else {
+        //    alert("HTTP-Error: " + response.status);
+        //}
+        console.log("complete try block");
     }
+    catch
+    {
+        let response = await fetch(url);
+
+        console.log("call to fetch api with relative url");
+        if (response.ok) { // if HTTP-status is 200-299
+            // get the response body (the method explained below)
+            let json = await response.json();
+            console.log(json);
+            alert("Response is " + json);
+        } else {
+            alert("HTTP-Error: " + response.status);
+        }
+        console.log("complete catch block");
+    }
+    console.log("finish");
+    return data;
 }
